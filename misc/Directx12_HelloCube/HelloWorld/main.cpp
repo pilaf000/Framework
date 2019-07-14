b319afe22b22aca76945f1639ad93ed4d1604c13
pilaf000 <ebipilaf000@gmail.com>
2019-07-15 02:33:42 +0900

    remove misc

--- misc/Directx12_HelloCube/HelloWorld/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "D3D12HelloWorld.h"
-
-_Use_decl_annotations_
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
-	D3D12HelloWindow sample(1280, 720, L"D3D12 Hello Window");
-	return Win32Application::Run(&sample, hInstance, nCmdShow);
-}
