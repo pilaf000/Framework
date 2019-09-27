6e5b5812f52217014559e40aa60958c6b4c89894
pilaf000 <ebipilaf000@gmail.com>
2019-09-28 02:02:17 +0900

    update:asset:文字コードが面倒だったのでコメント削除

--- assets/PS.hlsl
+++ assets/PS.hlsl
@@ -1,26 +1,26 @@
-﻿struct PSInput
+struct PSInput
 {
-	float4 position : SV_POSITION; 	// perspective変換後の頂点座標
-	float4 position_w : POSITION;  	// ワールド空間の頂点座標
-	float4 color : COLOR;		   	// 頂点カラー
-	float4 normal_w : NORMAL;	   	// ワールド空間の法線
-	float2 uv : TEXCOORD;		   	// テクスチャuv
+	float4 position : SV_POSITION;
+	float4 position_w : POSITION; 
+	float4 color : COLOR;		 
+	float4 normal_w : NORMAL;	  
+	float2 uv : TEXCOORD;
 };
 
 cbuffer MaterialInfo : register(b1)
 {
-	float3 baseColor;				// ベースカラー
-	float roughness;				// 表面の荒さ
-	float metallic;					// 金属感
-	float reflectance;				// 反射率
-	float2 pad;						// 使わない
-	float3 view;					// 視点座標
+	float3 baseColor;				
+	float roughness;				
+	float metallic;					
+	float reflectance;				
+	float2 pad;						
+	float3 view;					
 };
 cbuffer LightInfo : register(b2)
 {
-	float3 direction;				// ライトの方向
-	float3 lightColor;				// ライトのカラー
-	float intensity;				// ライトの強さ
+	float3 direction;				
+	float3 lightColor;
+	float intensity;				
 };
 
 Texture2D g_texture : register(t10);
@@ -28,5 +28,5 @@ SamplerState g_sampler : register(s0);
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-	return float4(0, 0, 0, 1);
+	return float4(0, 0, 0, 1.0);
 }
